a9f5d213e30f7063425fa5e4588101df7dc49093
Denis Leonenko <leograndprix@gmail.com>
2025-05-14 11:21:37 +0300

    Typed api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,20 +1,23 @@
 import { API_URL } from './config'
 
-const getAllCategories = async () => {
+import { Categories, Category } from '../types/categories'
+import { Meal } from '../types/meals'
+
+const getAllCategories = async (): Promise<Categories> => {
   const res = await fetch(API_URL + 'categories.php')
   const json = await res.json()
 
   return json.categories
 }
 
-const getFilteredByCategories = async (catName) => {
+const getFilteredByCategories = async (catName: string): Promise<Category> => {
   const res = await fetch(API_URL + 'filter.php?c=' + catName)
   const json = await res.json()
 
   return json.meals
 }
 
-const getMealByID = async (mealId) => {
+const getMealByID = async (mealId: string): Promise<Meal> => {
   const res = await fetch(API_URL + 'lookup.php?i=' + mealId)
   const json = await res.json()
 
